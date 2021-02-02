b4ce6d87c409e60c99bce8b77a881299ae381000
MasterCX <lnhldwzq@gmail.com>
2021-02-02 15:42:19 +1100

    add database

--- app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -3,31 +3,62 @@ package com.example.broadcastbestpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
-
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+    private CheckBox rememberPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPassword = (CheckBox) findViewById(R.id.remember_pass);
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
+        boolean isRemember = pref.getBoolean("remember_password", false);
+
+        if(isRemember){
+            String account = pref.getString("account","");
+            String password = pref.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPassword.setChecked(true);
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
 // 如果账号是admin且密码是123456，就认为登录成功
                 if (account.equals("admin") && password.equals("123456")) {
+                    editor = pref.edit();
+                    if(rememberPassword.isChecked()){
+
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                        editor.putBoolean("remember_password",true);
+                    }else {
+                        editor.clear();
+                    }
+                    editor.commit();
+
+
                     Intent intent = new Intent(LoginActivity.this,
                             MainActivity.class);
                     startActivity(intent);
--- app/src/main/java/com/example/broadcastbestpractice/MainActivity.java
+++ app/src/main/java/com/example/broadcastbestpractice/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.broadcastbestpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,12 +14,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 public class MainActivity extends BaseActivity {
+    private MyDatabaseHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+
         Button forceOffline = (Button) findViewById(R.id.force_offline);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        Button addData = (Button) findViewById(R.id.add_data);
+
+        addData.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values); // 插入第二条数据
+            }
+        });
+
+        createDatabase.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dbHelper.getWritableDatabase();
+            }
+        });
+
         forceOffline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
